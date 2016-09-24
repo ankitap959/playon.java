8985c7bf69b43bfa57155068235c610d3ec26ac4
vizsatiz <satis.vishnu@gmail.com>
2016-09-24 16:31:17 +0530

    Copletd the wireframe for data access layer. Will use the design pattern to continue

---
+++ app/src/main/java/com/archaea/dal/AuthenticationAdapter.java
@@ -0,0 +1,13 @@
+package com.archaea.dal;
+
+/**
+ * Created by vizsatiz on 24-09-2016.
+ */
+public class AuthenticationAdapter {
+
+    public AuthenticationAdapter(boolean isMock)
+    {
+
+    }
+
+}
---
+++ app/src/main/java/com/archaea/restclient/HttpMockRestClient.java
@@ -0,0 +1,7 @@
+package com.archaea.restclient;
+
+/**
+ * Created by vizsatiz on 24-09-2016.
+ */
+public class HttpMockRestClient implements IHttpRestClient{
+}
