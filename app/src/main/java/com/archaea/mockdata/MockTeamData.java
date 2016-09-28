36791c531af67bd55bd1a392443efb1d70febf17
vizsatiz <satis.vishnu@gmail.com>
2016-09-29 00:53:35 +0530

    Added members list to team response

--- app/src/main/java/com/archaea/mockdata/MockTeamData.java
+++ app/src/main/java/com/archaea/mockdata/MockTeamData.java
@@ -8,17 +8,7 @@ import org.json.JSONException;
  */
 public class MockTeamData {
 
-    private static String team = "[{\\\"name\\\":\\\"arsenal fc\\\",\\\"team_id\\\":1,\\\"team_guid\\\":\\\"" +
-            "89bb7dfc-7ba5-4ab6-8564-8fa0f08ac83c\\\",\\\"description\\\":\\\"My team is great at playing football" +
-            " and basketball\\\",\\\"team_metadata\\\":{\\\"sport_skills\\\":{\\\"football\\\":86,\\\"cricket\\\":" +
-            "75,\\\"batminton\\\":78,\\\"basketball\\\":50},\\\"average_fitness\\\":{\\\"running\\\":80,\\\"swimming\\\":60" +
-            ",\\\"cycling\\\":70}}},{\\\"name\\\":\\\"manu fc\\\",\\\"team_id\\\":2,\\\"team_guid\\\":\\\"71481901-1e7b" +
-            "-4c60-92d5-4994a0dc53bc\\\",\\\"description\\\":\\\"My team is great at playing football and cricket\\\"," +
-            "\\\"team_metadata\\\":{\\\"sport_skills\\\":{\\\"football\\\":36,\\\"cricket\\\":76,\\\"batminton\\\":58,\\\"" +
-            "basketball\\\":90},\\\"average_fitness\\\":{\\\"running\\\":80,\\\"swimming\\\":60,\\\"cycling\\\":70}}}]";
-
     /**
-     *
      [{
          "name": "arsenal fc",
          "team_id": 1,
@@ -37,6 +27,21 @@ public class MockTeamData {
          "cycling": 70
          }
          }
+         "members": [{
+         "email": "user.one@gmail.com",
+         "username": "userone",
+         "password": "playon@123",
+         "user_guid": "20080282-5fb6-4abb-af8e-246438bd5f43",
+         "profile_pic_link": "somelink ",
+         "facebook_token ": "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWIiOiIxMjM0NTY3ODkwIiwibmFtZSI6IkpvaG4gRG9lIiwiYWRtaW4iOnRydWV9.TJVA95OrM7E2cBab30RMHrHDcEfxjoYZgeFONFh7HgQ "
+         }, {
+         "email ": "user.two @gmail.com ",
+         "username": "usertwo",
+         "password": "playon@123",
+         "user_guid": "a6662028-d1f1-4ed7-8471-af8514e4337 a ",
+         "profile_pic_link ": "somelink ",
+         "facebook_token ": "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWIiOiIxMjM0NTY3ODkwIiwibmFtZSI6IkpvaG4gRG9lIiwiYWRtaW4iOnRydWV9.TJVA95OrM7E2cBab30RMHrHDcEfxjoYZgeFONFh7HgQ "
+         }]
          }, {
          "name": "manu fc",
          "team_id": 2,
@@ -55,10 +60,27 @@ public class MockTeamData {
          "cycling": 70
          }
          }
+         "members": [{
+         "email ": "user.two @gmail.com ",
+         "username": "usertwo",
+         "password": "playon@123",
+         "user_guid": "a6662028-d1f1-4ed7-8471-af8514e4337 a ",
+         "profile_pic_link ": "somelink ",
+         "facebook_token ": "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWIiOiIxMjM0NTY3ODkwIiwibmFtZSI6IkpvaG4gRG9lIiwiYWRtaW4iOnRydWV9.TJVA95OrM7E2cBab30RMHrHDcEfxjoYZgeFONFh7HgQ "
+         }, {
+         "email": "user.three@gmail.com",
+         "username": "userthree",
+         "password": "playon@123",
+         "user_guid ": "3a2a8bda-ed2b-4431-872b-a158811d5c6a",
+         "profile_pic_link ": "somelink ",
+         "facebook_token ": "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWIiOiIxMjM0NTY3ODkwIiwibmFtZSI6IkpvaG4gRG9lIiwiYWRtaW4iOnRydWV9.TJVA95OrM7E2cBab30RMHrHDcEfxjoYZgeFONFh7HgQ"
+         }]
      }]
-     * @return
-     * @throws JSONException
+     *
      */
+
+    private static String team = "[{\\\"name\\\":\\\"arsenal fc\\\",\\\"team_id\\\":1,\\\"team_guid\\\":\\\"89bb7dfc-7ba5-4ab6-8564-8fa0f08ac83c\\\",\\\"description\\\":\\\"My team is great at playing football and basketball\\\",\\\"team_metadata\\\":{\\\"sport_skills\\\":{\\\"football\\\":86,\\\"cricket\\\":75,\\\"batminton\\\":78,\\\"basketball\\\":50},\\\"average_fitness\\\":{\\\"running\\\":80,\\\"swimming\\\":60,\\\"cycling\\\":70}}\\\"members\\\":[{\\\"email\\\":\\\"user.one@gmail.com\\\",\\\"username\\\":\\\"userone\\\",\\\"password\\\":\\\"playon@123\\\",\\\"user_guid\\\":\\\"20080282-5fb6-4abb-af8e-246438bd5f43\\\",\\\"profile_pic_link\\\":\\\"somelink \\\",\\\"facebook_token \\\":\\\"eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWIiOiIxMjM0NTY3ODkwIiwibmFtZSI6IkpvaG4gRG9lIiwiYWRtaW4iOnRydWV9.TJVA95OrM7E2cBab30RMHrHDcEfxjoYZgeFONFh7HgQ \\\"},{\\\"email \\\":\\\"user.two @gmail.com \\\",\\\"username\\\":\\\"usertwo\\\",\\\"password\\\":\\\"playon@123\\\",\\\"user_guid\\\":\\\"a6662028-d1f1-4ed7-8471-af8514e4337 a \\\",\\\"profile_pic_link \\\":\\\"somelink \\\",\\\"facebook_token \\\":\\\"eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWIiOiIxMjM0NTY3ODkwIiwibmFtZSI6IkpvaG4gRG9lIiwiYWRtaW4iOnRydWV9.TJVA95OrM7E2cBab30RMHrHDcEfxjoYZgeFONFh7HgQ \\\"}]},{\\\"name\\\":\\\"manu fc\\\",\\\"team_id\\\":2,\\\"team_guid\\\":\\\"71481901-1e7b-4c60-92d5-4994a0dc53bc\\\",\\\"description\\\":\\\"My team is great at playing football and cricket\\\",\\\"team_metadata\\\":{\\\"sport_skills\\\":{\\\"football\\\":36,\\\"cricket\\\":76,\\\"batminton\\\":58,\\\"basketball\\\":90},\\\"average_fitness\\\":{\\\"running\\\":80,\\\"swimming\\\":60,\\\"cycling\\\":70}}\\\"members\\\":[{\\\"email \\\":\\\"user.two @gmail.com \\\",\\\"username\\\":\\\"usertwo\\\",\\\"password\\\":\\\"playon@123\\\",\\\"user_guid\\\":\\\"a6662028-d1f1-4ed7-8471-af8514e4337 a \\\",\\\"profile_pic_link \\\":\\\"somelink \\\",\\\"facebook_token \\\":\\\"eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWIiOiIxMjM0NTY3ODkwIiwibmFtZSI6IkpvaG4gRG9lIiwiYWRtaW4iOnRydWV9.TJVA95OrM7E2cBab30RMHrHDcEfxjoYZgeFONFh7HgQ \\\"},{\\\"email\\\":\\\"user.three@gmail.com\\\",\\\"username\\\":\\\"userthree\\\",\\\"password\\\":\\\"playon@123\\\",\\\"user_guid \\\":\\\"3a2a8bda-ed2b-4431-872b-a158811d5c6a\\\",\\\"profile_pic_link \\\":\\\"somelink \\\",\\\"facebook_token \\\":\\\"eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWIiOiIxMjM0NTY3ODkwIiwibmFtZSI6IkpvaG4gRG9lIiwiYWRtaW4iOnRydWV9.TJVA95OrM7E2cBab30RMHrHDcEfxjoYZgeFONFh7HgQ\\\"}]}]";
+
     public static JSONArray getTeamList() throws JSONException {
         return new JSONArray(team);
     }
