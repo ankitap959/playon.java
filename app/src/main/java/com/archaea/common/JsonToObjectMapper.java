2bfbf759ab666bd207e1e01d1486ecfa893a5e43
vizsatiz <satis.vishnu@gmail.com>
2016-10-13 00:53:09 +0530

    Changes for removing team-adapter chanegs

---
+++ app/src/main/java/com/archaea/common/JsonToObjectMapper.java
@@ -0,0 +1,19 @@
+package com.archaea.common;
+
+import com.archaea.models.User;
+
+import org.json.JSONObject;
+
+/**
+ * Created by vizsatiz on 29-09-2016.
+ */
+public class JsonToObjectMapper {
+
+    public static User toUser(JSONObject user) {
+        return null;
+    }
+
+    public static Team toTeam(JSONObject team){
+        return null;
+    }
+}
