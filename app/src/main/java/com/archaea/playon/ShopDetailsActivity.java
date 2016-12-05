e29d4bded3b8cffcabf72c0e084b2e9609f31a23
vizsatiz <satis.vishnu@gmail.com>
2016-12-06 00:21:59 +0530

    Made basic chnages for creating a basic shop details page.

--- app/src/main/java/com/archaea/playon/ShopDetailsActivity.java
+++ app/src/main/java/com/archaea/playon/ShopDetailsActivity.java
@@ -1,9 +1,9 @@
 package com.archaea.playon;
 
 import android.content.Context;
-import android.content.Intent;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
@@ -19,15 +19,15 @@ public class ShopDetailsActivity extends AppCompatActivity {
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
-        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.shoping_details_fab);
+        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
         fab.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Intent intent = new Intent(context, BookingActivity.class);
-                intent.putExtra("shopGuid", "Vishnu");
-                startActivity(intent);
+                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
+                        .setAction("Action", null).show();
             }
         });
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
     }
 
 }
